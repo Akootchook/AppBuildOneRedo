9dfbc07df80c43abbb1fb07b012a606b67730ee7
Akootchook <77140613+Akootchook@users.noreply.github.com>
2021-02-14 18:49:59 -1000

    Added About UHWO text.

--- AppBuildOneRedo/ViewController.swift
+++ AppBuildOneRedo/ViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+   
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    @IBOutlet weak var aboutText: UITextView!
+    override func viewDidLoad(){
         super.viewDidLoad()
+        
+   self.firstLabel.text = "University of Hawai'i West O'ahu"
+
+        self.aboutText.text = "UH West Oʻahu offers unique programs such as creative media, cybersecurity, health sciences, hospitality and tourism, and sustainable community food systems – developed to address workforce needs and leverage today’s technologies to train the next generation of community leaders."
         // Do any additional setup after loading the view.
     }
 
